257a8a678c60033d02fbec56c6da26d6436e772a
Teto-Novala <tetocoding@gmail.com>
2025-11-22 11:54:02 +0700

    feat: add and get room service

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function rooms(){
+        return $this->belongsToMany(Room::class,'room_user','room_id','user_id')->withTimestamps()->withPivot('joined_at');
+    }
 }
