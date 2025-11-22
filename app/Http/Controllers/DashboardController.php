63a0aa173443990ed233f03f7c561a8f7305df75
Teto-Novala <tetocoding@gmail.com>
2025-11-22 15:18:18 +0700

    feat: add model message and last message

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     public function index(RoomService $roomService): Response
     {
         $rooms = $roomService->getRooms();
-        
+
         dd($rooms);
         return Inertia::render('Index');
     }
