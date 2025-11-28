dc269432d258b9c24affedcfe77d3f0edb369476
Teto-Novala <tetocoding@gmail.com>
2025-11-28 20:02:49 +0700

    feat: add last message component

--- resources/js/Components/List/Item.jsx
+++ resources/js/Components/List/Item.jsx
@@ -1,15 +1,20 @@
+import LastMessage from "./LastMessage";
+
 const Item = ( {room } ) => {
+    console.log(room);
+    
 
   return (
-    <div className="py-3 px-3 flex rtl:space-x-reverse cursor-pointer hover:bg-gray-100 hover:rounded-lg dark:hover:bg-gray-800 rounded-lg">
+    <div className="text-white py-3 px-3 flex rtl:space-x-reverse cursor-pointer hover:bg-gray-100/50 hover:rounded-lg dark:hover:bg-gray-800 rounded-lg">
       <div className="flex-shrink-0">
           <img src={`images/${room.avatar}`} className="h-10 w-10 rounded-full bg-white" alt={room.name}></img>
       </div>
       <div className="flex-1 ml-2 w-0">
         <div>
-          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+          <p className="text-sm font-medium text-white truncate dark:text-white">
             {room.name}
           </p>
+          <LastMessage lastMessage={room.lastMessage.content}/>
         </div>
       </div>
     </div>
---
+++ resources/js/Components/List/LastMessage.jsx
@@ -0,0 +1,12 @@
+
+
+const LastMessage = ({ lastMessage }) => {
+
+  return (
+    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+      {lastMessage}
+    </p>
+  );
+}
+
+export default LastMessage;
